e56fe8eac0e71598da67a42b203456cfcbf93994
Psychee <15145032896@163.com>
2017-08-06 14:47:59 +0800

    字体图标更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PageHome from '@/components/page/PageHome'
+import PageHome from '@/components/page/PageHome'//首页
 import PageReg from '@/components/page/PageReg'
 import PageLogin from '@/components/page/PageLogin'
 import PageCollect from '@/components/page/PageCollect'
-import PageCart from '@/components/page/PageCart'
-import PageOrder from '@/components/page/PageOrder'
-import PageDetails from '@/components/page/PageDetails'
+import PageCart from '@/components/page/PageCart'//购物车
+import PageOrder from '@/components/page/PageOrder'//订单
+import PageDetails from '@/components/page/PageDetails'//详情页
+import PageUser from '@/components/page/PageUser'//个人主页
+import PageAddress from '@/components/page/PageAddress'//地址管理
+
 import MyTest from '@/components/MyTest'
 
 Vue.use(Router)
@@ -46,8 +49,16 @@ export default new Router({
       component: PageOrder
     },
     {
-      path:'/details',
+      path:'/details/:tag',
       component: PageDetails
+    },
+    {
+      path:'/user',
+      component: PageUser
+    },
+    {
+      path:'/user/address',
+      component: PageAddress
     }
   ]
 })
