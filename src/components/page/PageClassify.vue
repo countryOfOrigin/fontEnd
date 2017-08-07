e678be4b7c67f07ebfae856bdc1b4e81aeb027e0
Psychee <15145032896@163.com>
2017-08-07 15:59:43 +0800

    遗漏

---
+++ src/components/page/PageClassify.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-classify">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'page-classify',
+  data () {
+    return {
+
+
+    }
+  },
+  components:{
+
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
