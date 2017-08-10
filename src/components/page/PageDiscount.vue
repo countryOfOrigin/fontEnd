ea50ef40df3ac79c126d5db550bb43233655eae8
supremeyes <18646048278@163.com>
2017-08-10 10:49:30 +0800

    shoping-cart网页文件

--- src/components/page/PageDiscount.vue
+++ src/components/page/PageDiscount.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="page-discount">
-
+    <div id="hello-c">
+  hello c
+    </div>
   </div>
 </template>
 
@@ -22,5 +24,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  @import "../../assets/css/page/discount.css";
+  @import "../../assets/font/iconfont.css";
 
 </style>
