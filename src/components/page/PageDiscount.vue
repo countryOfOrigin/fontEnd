94ac5040cb67052de66a894e5f922a78a62567fe
lfucheng <1462168380@qq.com>
2017-08-17 16:38:30 +0800

    discount空页面

--- src/components/page/PageDiscount.vue
+++ src/components/page/PageDiscount.vue
@@ -28,88 +28,97 @@
         </div>
       </div>
     </div> -->
-    <div class="discount-box">
-      <div class="discount-left">
-        <span class="circles"></span>
-        <p>代金券</p>
-      </div>
-      <div class="discount-right">
-        <span class="circless"></span>
-        <div class="discount-price">
-          <div class="discount-price-left">
-            100元
+    <div v-if="flag">
+      <div class="discount-box">
+        <div class="discount-left">
+          <span class="circles"></span>
+          <p>代金券</p>
+        </div>
+        <div class="discount-right">
+          <span class="circless"></span>
+          <div class="discount-price">
+            <div class="discount-price-left">
+              100元
+            </div>
+            <div class="discount-price-right">
+              <p>仅限美亚宝3月期</p>
+              <p>单笔金额>10000元</p>
+            </div>
           </div>
-          <div class="discount-price-right">
-            <p>仅限美亚宝3月期</p>
-            <p>单笔金额>10000元</p>
+          <div class="discount-footer">
+            <div class="discount-footer-left">
+              注册获取
+            </div>
+            <div class="discount-footer-right">
+              <span class="glyphicon glyphicon-time"></span>
+              2016/6/7~6/12有效
+            </div>
+
           </div>
         </div>
-        <div class="discount-footer">
-          <div class="discount-footer-left">
-            注册获取
+      </div>
+      <div class="discount-box">
+        <div class="discount-left">
+          <span class="circles"></span>
+          <p>代金券</p>
+        </div>
+        <div class="discount-right">
+          <span class="circless"></span>
+          <div class="discount-price">
+            <div class="discount-price-left">
+              20元
+            </div>
+            <div class="discount-price-right">
+              <p>仅限美亚宝6月期</p>
+              <p>单笔金额>5000元</p>
+            </div>
           </div>
-          <div class="discount-footer-right">
-            <span class="glyphicon glyphicon-time"></span>
-            2016/6/7~6/12有效
-          </div>
+          <div class="discount-footer">
+            <div class="discount-footer-left">
+              首次获取
+            </div>
+            <div class="discount-footer-right">
+              <span class="glyphicon glyphicon-time"></span>
+              2016/6/7~6/12有效
+            </div>
 
+          </div>
+        </div>
+      </div>
+      <div class="discount-box">
+        <div class="discount-left">
+          <span class="circles"></span>
+          <p>代金券</p>
+        </div>
+        <div class="discount-right">
+          <span class="circless"></span>
+          <div class="discount-price">
+            <div class="discount-price-left">
+              50元
+            </div>
+            <div class="discount-price-right">
+              <p>仅限美亚宝12月期</p>
+              <p>单笔金额>3000元</p>
+            </div>
+          </div>
+          <div class="discount-footer">
+            <div class="discount-footer-left">
+              好友首次获取
+            </div>
+            <div class="discount-footer-right">
+              <span class="glyphicon glyphicon-time"></span>
+              2016/6/7~6/12有效
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
-    <div class="discount-box">
-      <div class="discount-left">
-        <span class="circles"></span>
-        <p>代金券</p>
-      </div>
-      <div class="discount-right">
-        <span class="circless"></span>
-        <div class="discount-price">
-          <div class="discount-price-left">
-            20元
-          </div>
-          <div class="discount-price-right">
-            <p>仅限美亚宝6月期</p>
-            <p>单笔金额>5000元</p>
-          </div>
-        </div>
-        <div class="discount-footer">
-          <div class="discount-footer-left">
-            首次获取
-          </div>
-          <div class="discount-footer-right">
-            <span class="glyphicon glyphicon-time"></span>
-            2016/6/7~6/12有效
-          </div>
-
-        </div>
-      </div>
-    </div>
-    <div class="discount-box">
-      <div class="discount-left">
-        <span class="circles"></span>
-        <p>代金券</p>
-      </div>
-      <div class="discount-right">
-        <span class="circless"></span>
-        <div class="discount-price">
-          <div class="discount-price-left">
-            50元
-          </div>
-          <div class="discount-price-right">
-            <p>仅限美亚宝12月期</p>
-            <p>单笔金额>3000元</p>
-          </div>
-        </div>
-        <div class="discount-footer">
-          <div class="discount-footer-left">
-            好友首次获取
-          </div>
-          <div class="discount-footer-right">
-            <span class="glyphicon glyphicon-time"></span>
-            2016/6/7~6/12有效
-          </div>
-
-        </div>
+    <div v-else>
+      <div class="empty-list">
+        <h4>心疼你，一张优惠券都木有 T.T</h4>
+        <p>快去首页发现优惠券</p>
+        <router-link to="/home">去找找</router-link>
       </div>
     </div>
   </div>
@@ -122,6 +131,7 @@ export default {
   name: 'page-discount',
   data () {
     return {
+      flag:false,
       discountList:[], 
       user_id:0,
     }
